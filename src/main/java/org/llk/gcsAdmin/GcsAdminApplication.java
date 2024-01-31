6eae0f493ee4788206aad95063a411912e0041fa
ilook <ilook12138@gmail.com>
2024-01-31 23:22:28 +0900

    init

---
+++ src/main/java/org/llk/gcsAdmin/GcsAdminApplication.java
@@ -0,0 +1,13 @@
+package org.llk.gcsAdmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcsAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcsAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/llk/gcsAdmin/GcsAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package org.llk.gcsAdmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcsAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
